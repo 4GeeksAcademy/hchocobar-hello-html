e313586d5e5e727a6994cdf76dd9a0dce9c989d9
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-07-15 17:51:51 +0000

    ulitimo

---
+++ index.js
@@ -0,0 +1,50 @@
+// Este es mi primer comentario de una línea en JavaScript
+
+/* Tipos de datos de JS simples  */
+let line = "Hola Mundo"  // string
+let lineDos = 'Hello world'  // string
+let text = '10'  // string
+let number = 10  // numérico entero - integer
+let decimal = 15.5  // numérico flotante, decimal, real o racional
+let verdadero = true  // booleano
+let falso = false  // boleano
+let noVariable = null  // valor nulo
+let noValor
+
+
+/* Tipos de datos de JS complejos */
+let miArray = [10, 'hola', 25, true, "mundo", false]  // array
+let person = {name: 'Manuel', age: 36, gender: 'male'}  // objetos: objeto literal
+// objetos: function  -- Lo vemos luego
+
+/* Operadores */
+// = Operador de asignación
+
+// Operadores matemáticos o aritméticos
+// + suma
+// - resta
+// * multiplicación 
+// / división
+// % módulo o resto 
+// // división entera
+
+// Operación de comparación
+// == iguladad
+// > mayor
+// < menor
+// >= mayor o igual que
+// <= menor o igual que
+// != distinto
+// === igual tipo o contenido
+// !== distinto en tipo o contenido
+
+// Operadores lógicos
+// && and 
+// || or
+// ! not
+
+// Operadores de string
+// + concatenación
+
+console.log('*******************************', lineDos, text, number);  // igualdad
+// console.log(person.name);
